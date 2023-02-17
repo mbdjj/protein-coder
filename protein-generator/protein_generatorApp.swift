a1c1407e5330f24dc0ce5503adc7b49d846070c6
Marcin Bartmiński <mbdjj@o2.pl>
2023-02-17 10:50:00 +0100

    Initial Commit

---
+++ protein-generator/protein_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  protein_generatorApp.swift
+//  protein-generator
+//
+//  Created by Marcin Bartminski on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct protein_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
